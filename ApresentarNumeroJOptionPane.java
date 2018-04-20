ff7aa603f32ca18b4cb8e1c280f941b8a2b157e1
wagner eduardo de melo monteiro <wagneeredu@gmail.com>
2018-04-20 09:06:50 -0300

    sei la

--- ApresentarNumeroJOptionPane.java
+++ ApresentarNumeroJOptionPane.java
@@ -4,7 +4,7 @@ public class ApresentarNumeroJOptionPane{
 
      public static void main(String[] args);
 
-         int numero = Integer.parseInt(JOptionPane.showInputDialog("Numero"));
+         int numero = Integer.parseInt(JOptionPane.showInputDialog("Nume2ro"));
 
 
 
